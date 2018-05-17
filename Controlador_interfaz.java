77f64086b770066faa4352442f43afd6edd270e1
paumg <38279644+paumg@users.noreply.github.com>
2018-05-17 13:14:13 +0200

    Add files via upload

---
+++ Controlador_interfaz.java
@@ -0,0 +1,36 @@
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ResourceBundle;
+ 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Controlador_interfaz implements Initializable {
+	
+	private Button registrar_jugador;
+	public TextField nombre_jugador;
+	public TextField pass_jugador;
+	
+	public void initialize(URL location, ResourceBundle resources) {
+		
+	}
+	
+	public void crearJugador(ActionEvent event){
+		System.out.println("boton clicado");
+		
+		Administrador player=new Administrador();
+		Controlador cont=new Controlador();
+		
+		String nombre= nombre_jugador.getText();
+		String pass=pass_jugador.getText();
+		
+		player.crearUsuarioJugador(nombre, pass);
+		cont.crearUsuarioJugador(player);
+	}
+
+}
